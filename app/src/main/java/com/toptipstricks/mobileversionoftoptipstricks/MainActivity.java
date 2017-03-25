6151768ba94b621a302854432f1a3be216227edb
satya <satyapriyapanda1993@gmail.com>
2017-03-26 00:58:33 +0530

    Initial commit

---
+++ app/src/main/java/com/toptipstricks/mobileversionoftoptipstricks/MainActivity.java
@@ -0,0 +1,38 @@
+package com.toptipstricks.mobileversionoftoptipstricks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView= (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://www.toptipstricks.com");
+        //this line will not allow to goto broweser for the link,it will open in here webview
+        mywebView.setWebViewClient(new WebViewClient());
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack())
+        {
+            mywebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+}
